749eda28b25a418f4f65c11a2bd3b31c6a9b7f8f
Rishabh Raj <176932192+Rishabh751@users.noreply.github.com>
2026-09-09 00:36:44 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -2,6 +2,13 @@ import java.text.NumberFormat;
 import java.util.Locale;
 class Solution {
     public int countCommas(int n) {
-        return Math.max(0,n-999);
+        // return Math.max(0,n-999);
+        int count=0;
+        int threshold=1000;
+        while(n>=threshold){
+            count+=(n-threshold+1);
+            threshold*=1000;
+        }
+        return count;
     }
 }
